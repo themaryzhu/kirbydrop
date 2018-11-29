791d256629dad0772833f8613eed263e7e059486
Andrea Dahl <ahdahl@stanford.edu>
2018-11-28 21:41:36 -0800

    created ball and paddles

--- gl.c
+++ gl.c
@@ -68,6 +68,17 @@ void gl_draw_rect(int x, int y, int w, int h, color_t c)
     }
 }
 
+// xLoc and yLoc are the center coordinates of the ball
+void gl_draw_ball(int xLoc, int yLoc, int radius, color_t c){
+    for(int y = -radius; y <= radius; y++) {
+        for(int x = -radius; x <= radius; x++) {
+            if(((x*x)+(y*y)) <= (radius*radius)) {
+                gl_draw_pixel(xLoc + x, yLoc + y, c);
+            }
+        }
+    }
+}
+
 void gl_draw_char(int x, int y, int ch, color_t c)
 {
     int font_size = font_get_size();
